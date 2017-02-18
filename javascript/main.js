418f4a3c72c6cdf75aac01e5cb47d73ae2fe72ae
Joshua Bradford <jjyet@yahoo.com>
2017-02-18 03:32:27 -0500

    initial commit, added temporary input box basic styling on name

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+// is there a tmdb database API we can access
+
+//make a tv show object
+//  1..name of show
+//     popularity
+//     runtime
+//     genre
+//     actors
+//     director
+//     writers
+
+//    how do we rank pop vs how much randomness
+//    can have it be sliding scale depending on number of rankings done
+
+//    address plan for saving user data / profiles
+
+var tvShow = {name:
+              popularity:
+              };
---
+++ readme.md
@@ -0,0 +1,22 @@
+###What you did###
+
+Skills practiced
+
+1. Javascript
+  * xxx
+  * xxx
+
+2. CSS
+  * xxx
+
+3. HTML
+  * xxx
+
+4. GitHub
+  * xxx
+
+5. Other
+  * xxx
+
+
+TO VIEW GOTO: https://farstrider777.github.io/color_clock/
