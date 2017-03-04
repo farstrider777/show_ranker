2893d23f7f14965df38d1bd3631f217de367676f
Joshua Bradford <jjyet@yahoo.com>
2017-03-04 09:32:53 -0500

    show ranker 1st algorythm over 680 shows

--- javascript/main.js
+++ javascript/main.js
@@ -19,39 +19,84 @@
 //url: `https://api.themoviedb.org/3/movie/551?api_key=c1590ac6ed33444a3c6284a9319516fe`,
 
 
-var info = [];
-var i = 1
+ var info = [];
+ var i = 1
 
-function fluff(data, fun, won){
+function fluff(data){
   //console.log(data)
 //  console.log(fun)
 //  console.log(won)
 //  console.log(data.results)
-  for(var count = 0; count < data.results.length; count++){
-    info.push(data.results[count])
+
+
+   for(var count = 0; count < data.results.length; count++){
+     info.push(data.results[count])
+     //console.log("indatapush")
   }
-  for(var count = 0; count < info.length; count++){
-    console.log(info[count].name);
-  }
-// console.log(info[1])
- i++;
- if(i < 4){
-  // console.log(i);
-   getData(fluff, i)
-  }
+  //  for(var count = 0; count < info.length; count++){
+  //   console.log(info[count].name);
+  //  }
+//
+// // console.log(info[1])
+   i++;
+  if(i < 35){
+ //  // console.log(i);
+ //
+   getData(i).then(fluff)
+ }else{
+
+
+
+
+var choice = 0;
+if (Math.random() < .75){
+  choice = Math.floor(Math.random()/4*680)
+  //console.log(choice)
+  console.log(choice)
+  console.log(info[choice].original_name)
+}else{
+
+  choice = Math.floor(Math.random()/2*680)
+  console.log(choice)
+  console.log(info[choice].original_name)
 }
 
-var search = "fight"
+var choice2;
+if (Math.random() < .25){
+  choice2 = Math.floor(Math.random()/4*680)
+  console.log(choice2)
+  console.log(info[choice2].original_name)
+}else if(Math.random() > .33){
+  choice2 = Math.floor(((Math.random()/2*680)+340))
+  //console.log("ruhere")
+  console.log(choice2)
+  console.log(info[choice2].original_name)
+}else{
+
+  choice2 = Math.floor(Math.random()/2*680)
+  console.log(choice2)
+  console.log(info[choice2].original_name)
+}
+
+}
+
+  //  if(Math.random() < .75){
+  //    console.log(Math.floor(680 / (Math.random / 4)));
+  //  }
+}
+
+//var search = "fight"
 
 //&language=en-US
 //vote_average.gte
 
-function getData (test, pageNum) {
-   $.ajax({
+function getData (pageNum) {
+  //console.log(pageNum);
+  return $.ajax({
     //  url: `https://api.themoviedb.org/3/search/keyword?api_key=c1590ac6ed33444a3c6284a9319516fe&page=1`,
     //  url: `https://api.themoviedb.org/3/tv/1?api_key=c1590ac6ed33444a3c6284a9319516fe`,
     //  url: `https://api.themoviedb.org/3/search/tv?api_key=c1590ac6ed33444a3c6284a9319516fe&query=its+always+sunny`,
-       url: `https://api.themoviedb.org/3/discover/tv?api_key=${token}&vote_count.gte=10&page=2`,
+       url: `https://api.themoviedb.org/3/discover/tv?api_key=${token}&vote_count.gte=10&page=${pageNum}`,
        language: "en-US",                                //     c1590ac6ed33444a3c6284a9319516fe
        //page: "5",            //`${page}`     `${pageNum}`
        sort_by: "vote_average.asc",
@@ -62,7 +107,7 @@ function getData (test, pageNum) {
   //   data: {
   //      q: search
   //    },
-      success: test
+    //  success: test
   });
   console.log(pageNum);
 }
@@ -72,6 +117,6 @@ function getData (test, pageNum) {
 //https://api.themoviedb.org/3/search/keyword?api_key=<<api_key>>&page=1            &timezone=America/New_York
 //&include_null_first_air_dates=false
 
-getData(fluff, 1);
+getData(1).then(fluff);
 
 //console.log(token)
---
+++ javascript/notes_sean.js
@@ -0,0 +1,25 @@
+/// for new player for left side
+    //it should pick one 75% chance of picking top 25% show
+    //  and 25% percent top 50%
+
+/// for new player for right side
+    // it should have a 25 percent chance of top 25percent show
+    // 50 percnet ch;ance of top 50 percent show
+    // 25% chance bottom 50 %
+
+
+/// two options manual rank one show (put in box one or two?)
+///rank vs already ranked shows, midpoint moving up and down
+/// other option
+
+/// i haven't seen option
+
+/// two movies not on the list so far they go in the middle one in top 50% one
+/// in bottom 50%
+
+/// two movies on the list it top movie is chosen as best no change
+/// if bototm bmovie is chosen as best it moves on top of top movie
+
+/// one movie off the list one on
+/// if the movie off the list wins it goes directly above the one on the list
+/// if the moive on the list wins it stays where it is and the movie off the list goes halfway betwen the moive on the list and the bottom
